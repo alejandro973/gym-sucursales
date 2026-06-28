ed814bc657ef6a42e61b4de982e2e5ee847909cc
alejandro973 <os.carrizo@duocuc.cl>
2026-06-28 11:59:40 -0400

    config: parametrizar yml con variables de entorno

--- src/test/java/com/gym/sucursales/SucursalServiceTest.java
+++ src/test/java/com/gym/sucursales/SucursalServiceTest.java
@@ -16,11 +16,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class SucursalServiceTest {
 
     @Autowired
